a99f17831827989d81864e1d1735709a4004ac42
dendum <dendumych@gmail.com>
2023-11-17 20:44:30 +0100

    Task completed and tested

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            profile.setName(getClearInfo(fileInputStream));
+            profile.setAge(Integer.valueOf(getClearInfo(fileInputStream)));
+            profile.setEmail(getClearInfo(fileInputStream));
+            profile.setPhone(Long.valueOf(getClearInfo(fileInputStream)));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("In&Out don't work (only till 7pm))");
+        } catch (NumberFormatException e) {
+            System.out.println("Nope... Numbers with chars");
+        }
+        return profile;
     }
+
+    private String getClearInfo(FileInputStream stream) throws IOException {
+        String info = "";
+        int c;
+        while ((c = stream.read()) !=  '\n') {
+            info += (char) c;
+        }
+
+        info = info.substring(info.lastIndexOf(':') + 1).trim();
+        return info;
+    }
+
 }
